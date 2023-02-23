f03a019446a52666657b528682be6b552f23f760
ikromyusuf <ikromyusupov09@gmail.com>
2023-02-23 10:34:30 +0500

    changed instruction

--- if03.py
+++ if03.py
@@ -1,6 +1,7 @@
 def main(a):
     """
-    If the number is positive, increase it to 1, else decrease it to 2. If it is 0, assign 10.
+    If the number is positive, increase it to 1,if the number is negative decrease it to 2.
+    If it is 0, assign 10.
     Args:
         a: integer
     Returns:
